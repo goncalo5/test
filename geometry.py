d0b8fef25ed1f9d7a1c9d1629a121ff0ba526e37
Goncalo <goncalo.s.and@gmail.com>
2018-11-21 15:13:51 +0000

    add geometry

--- geometry.py
+++ geometry.py
@@ -5,18 +5,15 @@ from math import sqrt
 
 
 class Point(object):
-
     def __init__(self, *args):
-        if len(args) == 2:
+        args = args if len(args) == 2 else args[0]
+
+        try:
+            self.x = args.x
+            self.y = args.y
+        except AttributeError:
             self.x = args[0]
             self.y = args[1]
-        if len(args) == 1:
-            try:
-                self.x = args[0].x
-                self.y = args[0].y
-            except AttributeError:
-                self.x = args[0][0]
-                self.y = args[0][1]
 
     def dist(self, other):
         return sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
@@ -68,8 +65,33 @@ c1 = Point(0, 0)
 c2 = Point(1, 1)
 c3 = Point(1, 2)
 
-# c3 = Point(c2)
+c3 = Point(c2)
 # print c3
+
+
+class Edge(object):
+    def __init__(self, *args):
+        # print args
+        args = args if len(args) == 2 else args[0]
+        self.points = tuple(Point(arg) for arg in args)
+
+    def length(self):
+        return self.points[0].dist(self.points[1])
+
+    def __repr__(self):
+        return 'Edge(%s, %s)' % (self.points)
+
+    def __eq__(self, other):
+        if self.points == other.points or self.points[::-1] == other.points:
+            return True
+        return False
+
+
+# print Edge(c1, c2) == Edge(Point(0, 0), Point(1, 1))
+# print Edge(c1, c2).points
+# print Edge(c1, c2).length()
+# print Edge((c1, c2)).points
+# print Edge((c1, c2)).length()
 
 
 class Vector(object):
@@ -121,7 +143,6 @@ class Vector(object):
             x = self.x + other[0]
             y = self.y + other[1]
             return Vector(x, y)
-        print x, y
         if isinstance(other, Point):
             return Point(x, y)
         elif isinstance(other, Vector):
@@ -134,38 +155,84 @@ class Vector(object):
         return self.add(other)
 
 
-class Triangle(object):
+class Polygon(object):
     def __init__(self, *args):
-        self.points = tuple(Point(arg) for arg in args)
-        self.n = 3
+        self.name = self.__class__.__name__
+        print(self.name)
+        self.vertices = tuple(Point(arg) for arg in args)
+
+        self.calc_edges()
+        self.calc_area()
+        self.calc_perimeter()
+
+    def calc_edges(self):
+        self.edges = []
+        for i in range(self.n):
+            j = (i + 1) % self.n
+            self.edges.append(Edge(self.vertices[i], self.vertices[j]))
 
     def calc_area(self):
         # |Sxiyi+1 + xny1 - Sxi+1yi -x1yn| / 2
         # |x1y2+x2y3+x3y1-y1x2-y2x3-y3x1| / 2
         soma = 0
         for i in range(self.n - 1):
-            print 'x%sy%s' % (i, i + 1)
-            r = self.points[i].x * self.points[i + 1].y
-            print r
-            soma += r
-        soma += self.points[self.n - 1].x * self.points[0].y
-        return soma
+            soma += self.vertices[i].x * self.vertices[i + 1].y
+        soma += self.vertices[self.n - 1].x * self.vertices[0].y
+        for i in range(self.n - 1):
+            soma -= self.vertices[i + 1].x * self.vertices[i].y
+        soma -= self.vertices[0].x * self.vertices[-1].y
+        soma /= 2.
+        self.area = abs(soma)
+
+    def calc_perimeter(self):
+        self.perimeter = sum(edge.length() for edge in self.edges)
 
     def __repr__(self):
-        return 'Triangle(%s, %s, %s)' % (self.points)
+        string = '%s, ' * self.n % self.vertices
+        return '%s(%s)' % (self.name, string[:-2])
+
+    def __add__(self, other):
+        self.vertices = tuple(map(lambda vertice: vertice + other, self.vertices))
+        self.calc_edges()
+        return self
+
+
+class Triangle(Polygon):
+    n = 3
+
+    def __init__(self, *args):
+        super(Triangle, self).__init__(*args)
+
+
+class Quadrilateral(Polygon):
+    n = 4
+
+    def __init__(self, *args):
+        super(Quadrilateral, self).__init__(*args)
 
 
 c1 = Point(0, 0)
 c2 = Point(0, 1)
 c3 = Point(1, 1)
+c4 = Point(1, 0)
 v1 = Vector(0, 1)
 v = []
-# v.append(Vector(0, 1))
+v.append(Vector(0, 1))
 
 
 t1 = Triangle(c1, c2, c3)
+
+print(t1)
+print(t1.n)
+print(t1.area)
+print(t1.perimeter)
+
+t1 = Quadrilateral(c1, c2, c3, c4)
 print t1
-print t1.calc_area()
-t1 = Triangle(c3, c2, c1)
-print t1
-print t1.calc_area()
+
+t1 = t1 + v1
+
+print(t1)
+print(t1.n)
+print(t1.area)
+print(t1.perimeter)
--- test.py
+++ test.py
@@ -1,3 +1,33 @@
-p = (1, 2)
+def test(x, y):
+    return x + y * (y > 0)
 
-print '%s, %s' % p
+
+a = [1]
+soma = reduce(lambda x, y: x + y, a)
+print soma
+n_elem = reduce(lambda x, y: x + 1, [1] + a[1:])
+print n_elem
+media = float(soma) / n_elem
+print media
+diff = map(lambda x: x - media, a)
+print diff
+diff_square = map(lambda x: x ** 2, diff)
+print diff_square
+variancia = reduce(lambda x, y: x + y, diff_square)
+print variancia
+desvio_padrao = variancia ** .5
+print 'desvio_padrao', desvio_padrao
+
+print
+media = float(sum(a)) / len(a)
+print media
+
+
+diff_square = map(lambda x: (x - media)**2, a)
+desvio_padrao = sum(diff_square) ** .5
+print desvio_padrao
+
+variancia = 0
+for value in a:
+    variancia += (value - media)**2
+print variancia ** 0.5
